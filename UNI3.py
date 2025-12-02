3581cfae64a6779a8ba8d6fc10d89dbc94229fb7
Go0MiShA <mirazizbek06@gmail.com>
2025-12-02 23:27:25 +0500

    Add files via upload

---
+++ UNI3.py
@@ -0,0 +1,40 @@
+import turtle
+
+koko = turtle.Turtle()
+
+screen = turtle.Screen()
+
+screen.bgcolor('Purple')
+koko.speed(5)
+koko.shape('triangle')
+koko.color("Silver")
+
+for loop in range(5):
+    koko.forward(100)
+    koko.right(72)
+
+for loop in range(4):
+    koko.forward(100)
+    koko.left(90)
+
+koko.forward(100)
+koko.left(90)
+
+for loop in range(5):
+    koko.forward(100)
+    koko.right(72)
+
+koko.forward(100)
+koko.left(90)
+
+for loop in range(5):
+    koko.forward(100)
+    koko.right(72)
+
+koko.forward(100)
+koko.left(90)
+
+for loop in range(5):
+    koko.forward(100)
+    koko.right(72)
+
